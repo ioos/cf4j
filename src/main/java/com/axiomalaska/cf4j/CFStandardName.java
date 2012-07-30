0f48a8bb36b44cd555662784ac89c76eb8bd0590
Shane StClair <shane@axiomalaska.com>
2012-07-30 11:43:16 -0700

    A few more javadoc comments

--- src/main/java/com/axiomalaska/cf4j/CFStandardName.java
+++ src/main/java/com/axiomalaska/cf4j/CFStandardName.java
@@ -1,5 +1,9 @@
 package com.axiomalaska.cf4j;
 
+/**
+ * Simple class to hold info for CF standard variable names.
+ * @see <a href="http://cf-pcmdi.llnl.gov/documents/cf-standard-names/">http://cf-pcmdi.llnl.gov/documents/cf-standard-names/</a>.  
+ */
 public class CFStandardName implements Comparable<CFStandardName>{
     private String name;
     private String canonicalUnits;
@@ -35,12 +39,22 @@ public class CFStandardName implements Comparable<CFStandardName>{
     public void setDescription(String description) {
         this.description = description;
     }
+    
+    /**
+     * Get the GRIB name for this variable, if one exists.
+     * @return the GRIB equivalent name
+     */    
     public String getGrib() {
         return grib;
     }
     public void setGrib(String grib) {
         this.grib = grib;
     }
+    
+    /**
+     * Get the Atmosphereic Model Intercomparison Project (AMIP) name for this variable, if one exists.
+     * @return the AMIP equivalent name
+     */
     public String getAmip() {
         return amip;
     }
